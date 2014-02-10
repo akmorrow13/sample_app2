9aed6b9a0af31a190396ab468b287a0b2fad1fd9
akmorrow13 <morrowalyssa@gmail.com>
2014-02-09 22:12:02 -0800

    home page 2

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,11 @@ SampleApp2::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'static_pages#home'
+  #  root 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
+  root :to => "static_pages#home"
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
